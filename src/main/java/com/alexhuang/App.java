79aff501cbe489499ef27a3600bb3f958dd0bde7
alexhuang <styleboycn@gmail.com>
2017-12-14 23:13:42 +0800

    add log4j2.xml configuration

--- src/main/java/com/alexhuang/App.java
+++ src/main/java/com/alexhuang/App.java
@@ -1,13 +1,20 @@
 package com.alexhuang;
 
+//import java.util.logging.LogManager;-->this is in rt.jar
+//import org.apache.log4j.LogManager;-->this is in log4j-1.2.17.jar
+import org.apache.logging.log4j.LogManager;//->this class in log4j-api-2.8.2.jar
+import org.apache.logging.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	private static Logger logger = LogManager.getLogger(App.class);
+
+	public static void main(String[] args) {
+		logger.info("Hello World!");
+		logger.error("Hello World!");
+		logger.warn("Hello World!");
+	}
 }
