0c488d4171b08967e8d908806b74dd861342c0b9
734618 <734618@SF0001369557A.sf.com>
2018-06-07 17:58:32 +0800

    commit visitor pattern test code 2

--- src/main/java/com/alexhuang/patterndesign/visitor/Car.java
+++ src/main/java/com/alexhuang/patterndesign/visitor/Car.java
@@ -1,19 +1,46 @@
 package com.alexhuang.patterndesign.visitor;
 
-public class Car {
+import java.util.ArrayList;
+import java.util.List;
 
-	private Engine  engine = new Engine();  
-    private Body    body   = new Body();  
-    private Wheel[] wheels   
-        = { new Wheel("front left"), new Wheel("front right"),  
-            new Wheel("back left") , new Wheel("back right")  };
+public class Car {
+	
+	private List<IVisitor> visitorList = new ArrayList<>();
+	
+	private String carName;
+	private Engine engine = new Engine("hongniu engine");
+	private Body body = new Body("falali body");
+	private Wheel[] wheels = { 
+			new Wheel("front left wheel"),
+			new Wheel("front right wheel"), 
+			new Wheel("back left wheel"),
+			new Wheel("back right wheel") 
+			};
+	
+    public Car(String name) {  
+        this.carName = name;  
+    } 
     
-    public void accept(IVisitor visitor) {  
-        visitor.visit(this);  
-        engine.accept(visitor);  
-        body.accept(visitor) ;   
-        for (int i = 0; i < wheels.length; ++ i)  
-            wheels[i].accept(visitor);  
-    }  
+	public void accept(IVisitor visitor) {
+		visitorList.add(visitor);
+	}
+	
+	public void action() {
+		for (IVisitor visitor : visitorList) {
+			visitor.visit(this);
+			engine.accept(visitor);
+			body.accept(visitor);
+			for (int i = 0; i < wheels.length; ++i)
+				wheels[i].accept(visitor);
+		}
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	public void setCarName(String carName) {
+		this.carName = carName;
+	}
 	
 }
--- src/main/java/com/alexhuang/patterndesign/visitor/Engine.java
+++ src/main/java/com/alexhuang/patterndesign/visitor/Engine.java
@@ -1,7 +1,22 @@
 package com.alexhuang.patterndesign.visitor;
 
 public class Engine {
-	public void accept(IVisitor visitor) {  
-        visitor.visit(this);  
-    }
+	
+	private String engineName;
+	
+	public Engine(String name) {  
+        this.engineName = name;  
+    } 
+	
+	public void accept(IVisitor visitor) {
+		visitor.visit(this);
+	}
+
+	public String getEngineName() {
+		return engineName;
+	}
+
+	public void setEngineName(String engineName) {
+		this.engineName = engineName;
+	}
 }
