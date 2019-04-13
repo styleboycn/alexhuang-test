f631d60262e95905f2811d8d7795a6ec45c828be
734618 <styleboycn@gmail.com>
2019-04-13 23:37:39 +0800

    commit some test unit2

--- src/test/java/com/alexhuang/java8/OptionalTest1.java
+++ src/test/java/com/alexhuang/java8/OptionalTest1.java
@@ -39,58 +39,6 @@ public class OptionalTest1 {
 		}
 	}
 
-	public static void testbasic1() {
-		List list1 = Stream.of("one", "two", "three", "four")
-				.filter(e -> e.length() > 3)
-				.peek(e -> System.out.println("Filtered value: " + e))
-				.map(String::toUpperCase)
-				.peek(e -> System.out.println("Mapped value: " + e))
-				.collect(Collectors.toList());//print none if no this line!!!
-		System.out.println(list1);
-	}
-
-	public static void testbasic2() {
-		Stream.of("one", "two", "three", "four")
-				.filter(e -> e.length() > 3)
-				.peek(e -> System.out.println("Filtered value: " + e));
-//				.foreach(e -> System.out.println("Filtered value: " + e));
-	}
-	
-	public static void testbasic3() {
-		Stream.of("one", "two", "three", "four")
-				.filter(e -> e.length() > 3)
-				.forEach(e -> System.out.println(e));
-	}
-	
-	public static void testbasic4() {
-		Stream.of("one", "two", "three", "four")
-				.filter(e -> e.length() > 3)
-				.peek(e -> System.out.println("Filtered value: " + e))
-				.peek(e -> e.toUpperCase())
-				.peek(e -> System.out.println("Mapped value: " + e))
-				.collect(Collectors.toList());
-	}
-
-	public static void testbasic5() {
-		Student std1 = new Student("zhangsan");
-		Student std2 = new Student("lisi");
-		Student std3 = new Student("wangwu");
-		List<Student> studentList = Arrays.asList(std1, std2, std3);
-		
-		System.out.println("BEFORE CONVERT : ");
-		List<Student> convertList = studentList
-				.stream()
-				.filter(e -> e.getName().length() > 4)
-				.peek(e -> System.out.println(e.getName()))
-				.peek(Student::convertName)
-				.peek(e -> System.out.println(e.getConvertName()))
-				.collect(Collectors.toList());
-		
-		System.out.println("\nAFTER CONVERT : ");
-		convertList.forEach(e -> System.out.println(e.getConvertName()));
-	}
-
-
 	public static void test_Optional_ofNullable() {
 		Student std1 = new Student("zhangsan");
 		Student std2 = new Student("lisi");
