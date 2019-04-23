4ad37cec378ae33396690dcbc399e7b414ec43ad
734618 <styleboycn@gmail.com>
2019-04-23 23:05:53 +0800

    commit some test unit for function

--- src/test/java/com/alexhuang/java8/FunctionTest.java
+++ src/test/java/com/alexhuang/java8/FunctionTest.java
@@ -13,30 +13,46 @@ public class FunctionTest {
 
     public static void test_function() {
         FunctionTest functionTest = new FunctionTest();
-        System.out.println(functionTest.compute1(5,i -> i * 2,i -> i * i));//50
-        System.out.println(functionTest.compute2(5,i -> i * 2,i -> i * i));//100
+        System.out.println(functionTest.functionCompute1(5,i -> i * 2,i -> i * i));//50
+        System.out.println(functionTest.functionCompute2(5,i -> i * 2,i -> i * i));//100
+    }
+
+    public static void test_function_2() {
+        FunctionTest functionTest = new FunctionTest();
+        System.out.println(functionTest.functionCompute3(5, x -> x + 2));//plus
+        System.out.println(functionTest.functionCompute3(5, x -> x - 2));//sub
+        System.out.println(functionTest.functionCompute3(5, x -> x * 2));//mul
+        System.out.println(functionTest.functionCompute3(5, x -> x / 2));//div
     }
 
     public static void test_biFunction() {
         FunctionTest biFunctionTest = new FunctionTest();
-        System.out.println(biFunctionTest.compute(4,5,(a,b) -> a * b,a -> a * 2));
+        System.out.println(biFunctionTest.biFunctionCompute(4,5,(a,b) -> a * b,a -> a * 2));
     }
 
-    public int compute1(int i, Function<Integer,Integer> after, Function<Integer,Integer> before){
+    public int functionCompute1(int i, Function<Integer,Integer> after, Function<Integer,Integer> before){
         return after.compose(before).apply(i);
     }
 
-    public int compute2(int i, Function<Integer,Integer> before,Function<Integer,Integer> after){
+    public int functionCompute2(int i, Function<Integer,Integer> before,Function<Integer,Integer> after){
         return before.andThen(after).apply(i);
     }
 
-    public int compute(int a, int b, BiFunction<Integer,Integer,Integer> biFunction,
+    public int functionCompute3(int i, Function<Integer,Integer> function){
+        Integer result = function.apply(i);
+        return result;
+    }
+
+    public int biFunctionCompute(int a, int b, BiFunction<Integer,Integer,Integer> biFunction,
                        Function<Integer,Integer> function){
         return biFunction.andThen(function).apply(a,b);
     }
 
+
+
     public static void main(String[] args) {
 //        test_function();
-        test_biFunction();
+//        test_biFunction();
+        test_function_2();
     }
 }
